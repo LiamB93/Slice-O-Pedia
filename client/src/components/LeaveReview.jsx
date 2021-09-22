66d403ab402c02922653e5e5360708f2685febec
Liam Baker <liambaker@liams-mbp.myfiosgateway.com>
2021-09-22 11:05:29 -0400

    stuck

--- client/src/components/LeaveReview.jsx
+++ client/src/components/LeaveReview.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { useHistory } from "react-router";
 import { useParams } from "react-router";
-import Form from "./Form";
+
 
 
 const base = process.env.REACT_APP_AIRTABLE_BASE
@@ -15,29 +15,23 @@ const config = {
 };
 
 function LeaveReview(props) {
-  const [name, setName] = useState("");
+  // const [name, setName] = useState("");
   const [location, setLocation] = useState("");
   const [rating, setRating] = useState(0);
   const [pizzeria, setPizzeria] = useState({});
   const { pizzerias } = props
 
   const history = useHistory();
-  const { pizzeriaName } = useParams();
+  const { pizzeriaName, pizzeriaLocation } = useParams();
 
   useEffect(() => {
     const pizzeria = pizzerias.find(pizzeria => pizzeria.fields.name === pizzeriaName)
-    // console.log("pizza", pizzeria);
+    const loc = pizzerias.find(loc => loc.fields.location === pizzeriaLocation)
     setPizzeria(pizzeria);
-    //   const getPizzerias = async () => {
-    //     const res = await axios.get(`${URL}/${id}`, config);
-    //     const { fields } = res.data;
-    //     setName(fields.name);
-    //     setLocation(fields.location);
-    //     setRating(fields.rating);
-    //   };
-    //   getPizzerias();
-    //   // eslint-disable-next-line
+    setLocation(loc);
+
   }, [])
+
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -56,16 +50,17 @@ function LeaveReview(props) {
     <div>
       <h1>Leave a Review!</h1>
       <h3> {pizzeriaName} </h3>
-      <Form
-        name={name}
-        setName={setName}
-        location={location}
-        setLocation={setLocation}
-        rating={rating}
-        setRating={setRating}
-        handleSubmit={handleSubmit}
-        type={"Edit"}
-      />
+      <h4> {pizzeriaLocation}</h4>
+      <form onSubmit={handleSubmit}>
+        <label>Leave Review</label>
+        <input
+          type="number"
+          rating={rating}
+          setRating={setRating}
+        />
+        <br />
+        <button>Submit</button>
+      </form>
 
     </div>
   )
